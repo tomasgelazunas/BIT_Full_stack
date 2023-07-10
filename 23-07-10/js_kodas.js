e2942cdb083720be3d0eaba773cee65c78f14019
tomas gelazunas <tom.gelazunas@gmail.com>
2023-07-10 14:52:02 +0300

    JS

---
+++ 23-07-10/js_kodas.js
@@ -0,0 +1,25 @@
+//1 užduotis
+let a = 'Angelina';
+let b = 'Jolie';
+
+if(a.length <= b.length) {
+    console.log(a);
+} 
+else {
+    console.log(b);
+}
+
+//2 užduotis
+let vardas = 'Tomas'
+let pavarde = 'Gelazunas'
+let gmetai = '1985'
+let metai = '2023'
+
+console.log('Aš esu ' +vardas+' '+pavarde+'.'+' Man yra '+(metai-gmetai)+' metai(-ų).');
+
+//3 užduotis
+let aa = 'Angelina';
+let bb = 'Jolie';
+let rezultatas = aa.substring(5) + bb.substring(2);
+
+console.log(rezultatas);
