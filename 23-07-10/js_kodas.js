913b5beea25d7c686025d930735cfb6fba8b7fa5
tomas gelazunas <tom.gelazunas@gmail.com>
2023-07-10 15:25:01 +0300

    Changes

--- 23-07-10/js_kodas.js
+++ 23-07-10/js_kodas.js
@@ -15,11 +15,13 @@ let pavarde = 'Gelazunas';
 let gmetai = '1985';
 let metai = '2023';
 
-console.log('Aš esu '+ vardas +' '+ pavarde +'. Man yra '+ (metai - gmetai) +' metai(-ų).');
+console.log(`Aš esu ${vardas} ${pavarde}. Man yra ${metai - gmetai} metai(-ų).`);
+//Arba galima taip, su '' kabutėmis: console.log('Aš esu '+ vardas +' '+ pavarde +'. Man yra '+ (metai - gmetai) +' metai(-ų).');
+
 
 //3 užduotis
 let aa = 'Angelina';
 let bb = 'Jolie';
-let rezultatas = aa.substring(5) + bb.substring(2);
 
+let rezultatas = aa.substring(5) + bb.substring(2);
 console.log(rezultatas);
