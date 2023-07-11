935304851cd4983ff1df2385ea88b1af31f11eaf
tomas gelazunas <tom.gelazunas@gmail.com>
2023-07-11 09:20:30 +0300

    changes

--- 23-07-10/js_kodas.js
+++ 23-07-10/js_kodas.js
@@ -1,15 +1,19 @@
-//1 užduotis
+//********************** 1 užduotis **************************
+
 let a = 'Angelina';
 let b = 'Jolie';
 
 if(a.length <= b.length) {
     console.log(a);
-} 
+} else if(a.length === b.length){
+    console.log(`Vardas ir pavardė vienodo ilgio`);
+}
 else {
     console.log(b);
 }
 
-//2 užduotis
+//********************** 2 užduotis **************************
+
 let vardas = 'Tomas';
 let pavarde = 'Gelazunas';
 let gmetai = '1985';
@@ -19,9 +23,11 @@ console.log(`Aš esu ${vardas} ${pavarde}. Man yra ${metai - gmetai} metai(-ų).
 //Arba galima taip, su '' kabutėmis: console.log('Aš esu '+ vardas +' '+ pavarde +'. Man yra '+ (metai - gmetai) +' metai(-ų).');
 
 
-//3 užduotis
+//********************** 3 užduotis **************************
+
 let aa = 'Angelina';
 let bb = 'Jolie';
 
 let rezultatas = aa.substring(5) + bb.substring(2);
 console.log(rezultatas);
+
