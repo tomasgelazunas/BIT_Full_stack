289b30df4e95407b045c77064ef6ac30c711efc1
tomas gelazunas <tom.gelazunas@gmail.com>
2023-07-13 17:35:24 +0300

    New file

---
+++ JavaScript/23-07-13/js_kodas_4.js
@@ -0,0 +1,15 @@
+// 1. Nupieškite kvadratą iš “*”, kurio kraštines sudaro 100 “*”. Panaudokite css stilių, kad kvadratas ekrane atrodytų kvadratinis.
+// * * * * * * * * * * *
+// * * * * * * * * * * *
+// * * * * * * * * * * *
+// * * * * * * * * * * *
+// * * * * * * * * * * *
+// * * * * * * * * * * *
+// * * * * * * * * * * *
+// 2. Prieš tai nupieštam kvadratui nupieškite raudonas istrižaines.
+// 3. Metam monetą. Monetos kritimo rezultatą imituojam rand() funkcija, kur 0 yra herbas, o 1 - skaičius. Monetos metimo rezultatus išvedame į ekraną atskiroje eilutėje: “S” jeigu iškrito skaičius ir “H” jeigu herbas. Suprogramuokite tris skirtingus scenarijus kai monetos metimą stabdome:
+// Iškritus herbui;
+// Tris kartus iškritus herbui;
+// Tris kartus iš eilės iškritus herbui;
+
+// https://lt.wikipedia.org/wiki/Pirminis_skai%C4%8Dius
