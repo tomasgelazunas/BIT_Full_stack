8e0cd8c1b37f94d5dc087455d8a62f131299ff3b
tomas gelazunas <tom.gelazunas@gmail.com>
2023-07-14 09:50:39 +0300

    Homework 4

--- JavaScript/23-07-13/js_kodas_4.js
+++ JavaScript/23-07-13/js_kodas_4.js
@@ -13,3 +13,99 @@
 // Tris kartus iš eilės iškritus herbui;
 
 // https://lt.wikipedia.org/wiki/Pirminis_skai%C4%8Dius
+
+const rand = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+// first and second task
+
+let str = '';
+
+for (let i = 1; i <= 100; i++) {
+  if (Number.isInteger((i - 1) / 11)) {
+      str += '<span style="color: red;">*</span>';
+  } else if (i === (Math.ceil(i / 10) * 10) - Math.floor(i / 10) || i === 10) {
+      str += '<span style="color: red;">*</span>';
+  } else {
+      str += '<span>*</span>';
+  }
+
+  if (Number.isInteger(i / 10)) {
+      str += '<br>';
+  }
+}
+
+document.getElementById('stars-box').innerHTML = str;
+
+// third task
+// scene 1
+
+const n = id => document.querySelector(`#hm > #scene${id}`);
+const hs = 'HS';
+
+str = '';
+
+while (true) {
+  const randN = rand(0, 1);
+
+  str += hs[randN] + '<br>';
+
+  n(1).innerHTML = str;
+
+  if (randN === 0) break;
+}
+
+// scene 2
+
+let i = 0;
+let hmh = 0;
+
+str = '';
+
+while (true) {
+  const randN = rand(0, 1);
+
+  str += hs[randN] + '<br>';
+
+  n(2).innerHTML = str;
+
+  if (randN === 0) {
+      hmh++;
+  }
+
+  if (hmh === 3) break;
+
+  i++;
+}
+
+//scene 3
+
+i = 0;
+hmh = 0;
+str = '';
+
+let prvH = false;
+
+while (true) {
+  const randN = rand(0, 1);
+
+  if (randN === 0) {
+      hmh++;
+
+      prvH = true;
+  } else {
+      hmh = 0;
+
+      prvH = false;
+  }
+
+  str += hs[randN] + '<br>';
+
+  n(3).innerHTML = str;
+
+  if(hmh === 3) break;
+}
