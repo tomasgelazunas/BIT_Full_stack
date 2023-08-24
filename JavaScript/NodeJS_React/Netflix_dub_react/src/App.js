00d6dc926934866f5aa1074056ee1a1d348fccde
tomas gelazunas <tom.gelazunas@gmail.com>
2023-08-24 19:14:11 +0300

    Lower Sections not finished

--- JavaScript/NodeJS_React/Netflix_dub_react/src/App.js
+++ JavaScript/NodeJS_React/Netflix_dub_react/src/App.js
@@ -1,6 +1,11 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import Mainsect from "./components/Mainsect/Mainsect";
+import LowerSect from "./components/LowerSect/LowerSect";
+import TV from "./components/Images/tv.png";
+import Mobile from "./components/Images/mobile.jpg";
+import Devices from "./components/Images/devices.png";
+import Kids from "./components/Images/kids.png";
 
 function App() {
   return (
@@ -9,6 +14,32 @@ function App() {
         <Header />
         <Mainsect />
       </div>
+      <LowerSect
+        headingText="Enjoy on your TV"
+        pText="Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more."
+        src={TV}
+      />
+      <LowerSect
+        headingText="Download your shows to watch offline"
+        pText="Save your favorites easily and always have something to watch."
+        src={Mobile}
+        style={{
+          flexDirection: "row-reverse",
+        }}
+      />
+      <LowerSect
+        headingText="Watch everywhere"
+        pText="Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV."
+        src={Devices}
+      />
+      <LowerSect
+        headingText="Create profiles for kids"
+        pText="Send kids on adventures with their favorite characters in a space made just for them—free with your membership."
+        src={Kids}
+        style={{
+          flexDirection: "row-reverse",
+        }}
+      />
     </>
   );
 }
---
+++ JavaScript/NodeJS_React/Netflix_dub_react/src/components/LowerSect/LowerSect.js
@@ -0,0 +1,17 @@
+const LowerSect = (props) => {
+  return (
+    <section className="blackSection py-5">
+      <div className="container d-flex" style={props.style}>
+        <div className="col-6 d-flex flex-column justify-content-center">
+          <h2>{props.headingText}</h2>
+          <p>{props.pText}</p>
+        </div>
+        <div className="col-6">
+          <img src={props.src} className="img-fluid" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default LowerSect;
